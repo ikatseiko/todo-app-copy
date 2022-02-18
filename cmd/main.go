22dcedfee6c057beaffff20c767785b386a0873a
i.katseiko <i.katseiko@tha.kz>
2022-02-18 12:22:36 +0600

    lesson3
    внедрение зависимостей

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"github.com/ikatseiko/todo-app-copy"
 	"github.com/ikatseiko/todo-app-copy/pkg/handler"
+	"github.com/ikatseiko/todo-app-copy/pkg/repository"
+	"github.com/ikatseiko/todo-app-copy/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
