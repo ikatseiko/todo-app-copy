d283cde95498fbd203a3250ee897b45edcf884ac
i.katseiko <i.katseiko@tha.kz>
2022-02-18 14:59:30 +0600

    lesson-4
    файлы миграции

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,26 @@ import (
 	"github.com/ikatseiko/todo-app-copy/pkg/handler"
 	"github.com/ikatseiko/todo-app-copy/pkg/repository"
 	"github.com/ikatseiko/todo-app-copy/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initconfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err)
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initconfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
